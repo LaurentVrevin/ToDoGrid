70eb09abb0694b912fe7c192152e781b0e25f957
Laurent Vrevin <laurent.vrevin@hotmail.fr>
2024-03-20 16:04:54 +0100

    change theme and restore theme

--- app/src/main/java/com/laurentvrevin/todogrid/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/laurentvrevin/todogrid/presentation/ui/theme/Theme.kt
@@ -4,14 +4,12 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -65,12 +63,7 @@ fun ToDoGridTheme(
     }
 
     MaterialTheme(
-        colorScheme = lightColorScheme(
-            primary = Purple40,
-            secondary = PurpleGrey40,
-            tertiary = Pink40,
-            surface = Color.Transparent
-        ),
+        colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
